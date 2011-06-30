7f3783c2503aed2c3966c7a1d95c0b1f5dd8eedb
HadiM <marelo64@gmail.com>
2011-07-01 09:07:44 +1000

    01/07/2011- 8:52

---
+++ pygraphml/Point.py
@@ -0,0 +1,57 @@
+#-*- coding: utf-8 -*-
+
+import math
+
+class Point():
+    """
+    """
+
+    def __init__(self, x = 0, y = 0, z = 0):
+        """
+        """
+
+        self.x = float(x)
+        self.y = float(y)
+        self.z = float(z)
+
+    def vectorize(self, point):
+        """
+        """
+
+        p = Point()
+        
+        p.x = self.x - point.x
+        p.y = self.y - point.y
+        p.z = self.z - point.z
+
+        return p
+
+    def __mul__(self, point):
+        """
+        Cross product
+        """
+
+        p = Point()
+        
+        p.x = self.y * point.z - self.z * point.y
+        p.y = self.z * point.x - self.x * point.z
+        p.z = self.x * point.y - self.y * point.x
+
+        return p
+
+    def __str__(self):
+        """
+        """
+
+        s = ""
+        
+        s += "x : %f" % self.x
+        s += "\n"
+
+        s += "y : %f" % self.y
+        s += "\n"
+
+        s += "z : %f" % self.z
+        s += "\n"
+
+        return s
