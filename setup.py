ca80d413afd0299649dbf2061290ce04678ed904
Ari Lacenski <alacenski@gmail.com>
2013-02-26 11:51:27 -0800

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup(name='pygraphml',
+    version='1.0',
+    author='Hadrien Mary',
+    url='https://github.com/hadim/pygraphml/',
+    packages=['pygraphml'])
