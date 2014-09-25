a12b8f02b9d891b853c82fcd7397b4f6f9006017
Low Kian Seong <kianseong@gmail.com>
2014-09-25 17:14:43 +0800

    Update setup.py
    Changed description based on request.

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from distutils.core import setup
 setup(name='pygraphml',
     version='1.0',
     author='Hadrien Mary',
+    description='Small library to parse GraphML files in Python',
     url='https://github.com/hadim/pygraphml/',
     packages=['pygraphml'])
